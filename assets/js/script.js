c318366f816dbe9447efbfe6ffc8d0c894cb69d6
Paul Hendrickson <PaulRobHendrickson@gmail.com>
2019-11-26 14:39:28 -0600

    now displays funny quotes

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,30 @@ function initProgram() {
     const testingButtonEl = document.getElementById("testingButton");
     const mainflexEl = document.getElementById("mainflex");
     const moveForwardButtonEl = document.getElementById("moveForward");
-    const appNameEl = document.getElementById("appName")
-    const spacerEl = document.getElementById("spacer")
-    const dogImageEl = document.getElementById("dogImage")
-    const startOverEl = document.getElementById("startOver")
+    const appNameEl = document.getElementById("appName");
+    const spacerEl = document.getElementById("spacer");
+    const dogImageEl = document.getElementById("dogImage");
+    const startOverEl = document.getElementById("startOver");
+    const dogQuestions = [`	Have you ever used the snapchat dog filter and thought 'man, I wish I really did look this cute?'	`,
+        `	Have you ever looked at a can of Alpo and thought, 'These savory chunks? This is better than what I normally eat!'	`,
+        `	Has your wife ever kicked you to the 'dog house' and you were kind of okay with it?	`,
+        `	Ever see a dog in public and think, 'Hey, that kind of looks like me!'?	`,
+        `	Are you a furry? Do you want to be?	`,
+        `	Do you ever get so distracted and others around you have to yell 'squirrel' to remind you to get you back on track?	`,
+        `	Ever watch a dog show and think to yourself, 'I could do that!'	`,
+        `	Have you been called a Golden Retriever because of your personality? Me neither…	`,
+        `	Is your nose wet? 	`,
+        `	Does your hair get shiny after you eat scrambled eggs?	`,
+        `	Is your shampoo oatmeal-based?	`,
+        `	Does the sound of 'treat' or 'snack' get you excited?	`,
+        `	Does a red fire hydrant fill you with feels?	`,
+        `	Do you experience intense, heart-wrenching, uncontrollable feelings of abandonment whenever anyone leaves the room?	`,
+        `	Do you sit in a kennel when you’re at home by yourself?	`,
+        `	Is your favorite movie Holes (2003) starring Shia LaBeouf?	`,
+        `	Do you have the TV on when you’re home alone, just so it kind of feels like you’re with people because you’d get too sad otherwise?	`,
+        `	Do you gain an irresistible feeling to scratch the back of your ear with your foot?	`,
+        `   Are you a good boy?`,
+        `   `]
 
     //Adds an event listener to the button for testing
     //runs the applyTraitToDog function
@@ -26,7 +46,7 @@ function initProgram() {
         moveForward();
     })
 
-     //Adds an event listener to the move forward button
+    //Adds an event listener to the move forward button
     //runs the startOver function
     startOverEl.addEventListener("click", function () {
         startOver();
@@ -98,11 +118,35 @@ function initProgram() {
     //creates an element with the image tag
     //appends that information to the body
     function renderDogPic() {
-        const dogPicEl = document.createElement("img")
+        const dogPicEl = document.createElement("img");
         dogPicEl.setAttribute("src", informationGathered.data.message);
         document.body.append(dogPicEl);
     }
 
+    function renderDogQuestions() {
+        function shuffle(a) {
+            let j, x, i;
+            for (i = a.length - 1; i > 0; i--) {
+                j = Math.floor(Math.random() * (i + 1));
+                x = a[i];
+                a[i] = a[j];
+                a[j] = x;
+            }
+        } shuffle(dogQuestions)
+        console.log(dogQuestions);
+        
+        for (let i = 0; i < 4; i++) {
+            $("#dogQuestions").append(
+                
+                
+                `
+            <p class="dogQ`+i+`">` + dogQuestions[i] +`</p>
+            `
+        
+        
+            )
+        }
+    } renderDogQuestions();
 
 
 } initProgram();
