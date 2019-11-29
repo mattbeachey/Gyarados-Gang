b0ae2a84667be7b4c0ba0a33f9bcd2cb4d77c1d7
Elliot Trejo <elliot.trejo.tcu@gmail.com>
2019-11-29 15:11:00 -0600

    Was able to figure out how to coordinate the position on the map to St. Paul, currently working on an address input to convert address to long and lat.

--- assets/js/script.js
+++ assets/js/script.js
@@ -220,18 +220,22 @@ function initProgram() {
 
 
 } 
-initProgram();
+// initProgram();
 
 
 
 function initMap() {
+
+    
   // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
+  var stp = {lat: 44.954, lng: -93.091};
   // The map, centered at Uluru
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
+      document.getElementById('map'), {zoom: 4, center: stp});
   // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
+  var marker = new google.maps.Marker({position: stp, map: map});
+
+
 }
 
 function displayMaps (){
